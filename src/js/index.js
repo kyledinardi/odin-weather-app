3639732e868e4e2a5c20f40b101f223a62b221a9
Kdin <94251418+awesome13524@users.noreply.github.com>
2024-01-03 12:22:12 -0500

    Create function to fetch api data

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 import '../style.css';
+
+async function getWeather(location) {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=ebcf2e50162447c9b21182536233012&days=3&aqi=yes&alerts=yes&q=${location}`,
+    { mode: 'cors' },
+  );
+
+  const weatherData = await response.json();
+  return weatherData;
+}
+
+getWeather('Washington, D.C.').then(console.log);
